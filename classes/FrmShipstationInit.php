eabf5a1603dbfa4929ab31c2d85a7e1b66b9bee6
Stan Matrosov <matrosovdream@gmail.com>
2025-09-07 22:42:20 +0200

    Init class is updated, added cron and other classes

--- classes/FrmShipstationInit.php
+++ classes/FrmShipstationInit.php
@@ -12,12 +12,21 @@ class FrmShipstationInit {
         // Endpoints
         require_once FRM_SHP_BASE_URL.'/classes/endpoints/FrmShipstationRoutes.php';
 
+        // Migrations
+        $this->include_migrations();
+
+        // Models
+        $this->include_models();
+
+        // Utilities
+        $this->include_utils();
+
+        // CRON
+        $this->include_cron();
+
         /*
         // CRON
         require_once FRM_SHP_BASE_URL.'/classes/cron/schedules.cron.php';
-
-        // Migrations
-        $this->include_migrations();
 
         // Shortcodes
         $this->include_shortcodes();
@@ -31,7 +40,51 @@ class FrmShipstationInit {
     private function include_migrations() {
 
         // Entries cleaner extra tables
-        require_once FRM_SHP_BASE_URL.'/classes//migrations/archive.entries.php';
+        require_once FRM_SHP_BASE_URL.'/classes//migrations/FrmShipstationMigrations.php';
+
+        // Run migrations
+        FrmShipstationMigrations::maybe_upgrade();
+
+    }
+
+    private function include_models() {
+
+        // Abstract model
+        require_once FRM_SHP_BASE_URL.'/classes/models/FrmShipstationAbstractModel.php';
+
+        // Order model
+        require_once FRM_SHP_BASE_URL.'/classes/models/FrmShipstationOrderModel.php';
+
+        // Shipment model
+        require_once FRM_SHP_BASE_URL.'/classes/models/FrmShipstationShipmentModel.php';
+
+        // Carrier model
+        require_once FRM_SHP_BASE_URL.'/classes/models/FrmShipstationCarrierModel.php';
+
+        // Package model
+        require_once FRM_SHP_BASE_URL.'/classes/models/FrmShipstationPackageModel.php';
+
+        // Service model
+        require_once FRM_SHP_BASE_URL.'/classes/models/FrmShipstationServiceModel.php';
+
+    }
+
+    private function include_utils() {
+
+        // Model Entry
+        require_once FRM_SHP_BASE_URL.'/classes/utils/FrmShipstationModelEntry.php';
+
+    }
+
+    private function include_cron() {
+
+        // Orders cron
+        require_once FRM_SHP_BASE_URL.'/classes/cron/FrmShipstationOrdersCron.php';
+        FrmShipstationOrdersCron::init();
+
+        // Carriers cron
+        require_once FRM_SHP_BASE_URL.'/classes/cron/FrmShipstationCarriersCron.php';
+        FrmShipstationCarriersCron::init();
 
     }
 
